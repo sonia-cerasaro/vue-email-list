ddc8ccc134dc9a0cd43b5d4810eaa3e644dabb47
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-04-08 17:15:44 +0200

    completed

--- main.js
+++ main.js
@@ -2,18 +2,23 @@ var app = new Vue (
   {
     el: '#root',
     data: {
-      mail: '',
       mails: [],
     },
     methods: {
 
       getMail: function () {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((response) => {
-          console.log(response.data.response);
-          this.mail = response.data.response;
-        });
-     },
+        var receivedMails = [];
+        for (var i = 0; i < 10; i++) {
 
+          axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then((response) => {
+            receivedMails.push(response.data.response);
+            console.log(response.data.response);
+          });
+
+        }
+        this.mails = receivedMails;
+
+     },
     }
   });
